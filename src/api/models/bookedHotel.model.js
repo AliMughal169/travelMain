4425aca911ec6d922aa108e42f9b7d9d1fdb317c
abd-rehman12 <117512314+abd-rehman12@users.noreply.github.com>
2023-01-14 03:14:14 +0500

    modified

--- src/api/models/bookedHotel.model.js
+++ src/api/models/bookedHotel.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const bookedHotelSchema = new mongoose.Schema({
-    userId: { type: mongoose.Types.ObjectId, required: true },
-    hotelId: { type: mongoose.Types.ObjectId, required: true },
-    roomId: { type: mongoose.Types.ObjectId, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    hotelId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    roomId: { type: mongoose.Schema.Types.ObjectId, required: true },
     checkIn: { type: Date, required: true },
     checkOut: { type: Date, required: true },
     guestName: { type: String, required: true },

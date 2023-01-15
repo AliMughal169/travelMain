95a63853b61c4b504aacd0834e7f5a45c1887b6a
Salman Naeem <thesalmanofficiall@gmail.com>
2023-01-15 11:22:32 +0500

    admin complete

--- admin/src/views/BookedHotelDetails.js
+++ admin/src/views/BookedHotelDetails.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import { Button, Modal } from 'react-bootstrap'
-import axios from "axios";
-import { useEffect, useState } from "react";
 const backendUrl = process.env.REACT_APP_BASE_URL;
 
-function checkBooked() {
 
-}
 
-function BookedHotelDetails({ show, setShow, hotelDetail, roomData, hotelData }) {
-    // console.log(hotelDetail._id)
-    // console.log(roomData)
-    // console.log(hotelData.userId)
+function BookedHotelDetails({ show, setShow, hotelDetail, roomData, hotelData, hotelList }) {
+    const checkBookedRoom = () => {
+        for (let i = 0; i < roomData.length; i++) {
+
+            for (let j = 0; j < hotelData.length; j++) {
+
+
+                if (roomData[i].hotelId == hotelData[j].hotelId && roomData[i]._id == hotelData[j].roomId && hotelDetail._id == hotelData[j].userId) {
+                    return i;
+                }
+            }
+        }
+
+    }
+    const checkBookedHotel = () => {
+        for (let j = 0; j < hotelData.length; j++) {
+
+            if (hotelDetail._id == hotelData[j].userId) {
+                // console.log(j)
+                return j;
+            }
+        }
+
+    }
+    const checkHotelList = () => {
+        for (let i = 0; i < hotelList.length; i++) {
+            for (let j = 0; j < hotelData.length; j++) {
+                if (hotelList[i]._id == hotelData[j].hotelId) {
+                    return i;
+                }
+            }
+        }
+
+    }
 
     return (
         <>
@@ -33,29 +59,33 @@ function BookedHotelDetails({ show, setShow, hotelDetail, roomData, hotelData })
                     <div className='container-fluid'>
                         <h4>Hotel Details</h4>
                         {
-                            hotelData.map((data, index) => {
-                                return (
-                                    <div className=' row text-left '>
-                                        <div className='col-sm'>
-                                            <div className='label' >
-                                                <p className=' font-weight-bold'>Hotel Name : {hotelDetail.firstName} </p>
-                                            </div>
-                                            <div className='label' >
-                                                <p className=' font-weight-bold'>Guest Name :{data.guestName} </p>
-                                            </div>
-                                            <div className='label' >
-                                                <p className=' font-weight-bold'>Check In : {data.checkIn} </p>
-                                            </div>
-                                            <div className='label' >
-                                                <p className=' font-weight-bold'>Check Out : {data.checkOut} </p>
-                                            </div>
-                                            <div className='label' >
-                                                <p className=' font-weight-bold'>Booking Date : {data.bookingDate} </p>
-                                            </div>
+                            (checkBookedHotel() != undefined) ? (
+                                <div className=' row text-left '>
+                                    <div className='col-sm'>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Hotel Name : {hotelList[checkHotelList()].hotelName} </p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Guest Name :{hotelData[checkBookedHotel()].guestName} </p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Check In : {hotelData[checkBookedHotel()].checkIn} </p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Check Out : {hotelData[checkBookedHotel()].checkOut} </p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Booking Date : {hotelData[checkBookedHotel()].bookingDate} </p>
                                         </div>
                                     </div>
-                                )
-                            })
+                                </div>
+
+                            ) : (
+                                <div className='label' >
+                                    <p className=' font-weight-bold'>Booking Date : not mached </p>
+                                </div>
+                            )
+
                         }
 
 
@@ -64,26 +94,32 @@ function BookedHotelDetails({ show, setShow, hotelDetail, roomData, hotelData })
                         <h4>Room Detail</h4>
                         {/* {roomData === hotelDetail ? ( */}
                         {
+                            (checkBookedRoom() != undefined && checkBookedHotel() != undefined) ? (
 
-                            <div className=' row text-left '>
-                                <div className='col-sm'>
-                                    <div className='label' >
-                                        <p className=' font-weight-bold'>Category:  {roomData[0].category}</p>
-                                    </div>
-                                    <div className='label' >
-                                        <p className=' font-weight-bold'>Room Number :  {roomData[0].roomNumber}</p>
-                                    </div>
-                                    <div className='label' >
-                                        <p className=' font-weight-bold'>Price :  {roomData[0].price}</p>
-                                    </div>
-                                    <div className='label' >
-                                        <p className=' font-weight-bold'>Description :  {roomData[0].desc}</p>
-                                    </div>
-                                    <div className='label' >
-                                        <p className=' font-weight-bold'>Is Booked:  {roomData[0].isFull}</p>
+                                <div className=' row text-left '>
+                                    <div className='col-sm'>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Category:  {roomData[checkBookedRoom()].category}</p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Room Number :  {roomData[checkBookedRoom()].roomNumber}</p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Price :  {roomData[checkBookedRoom()].price}</p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Description :  {roomData[checkBookedRoom()].desc}</p>
+                                        </div>
+                                        <div className='label' >
+                                            <p className=' font-weight-bold'>Is Booked:  {roomData[0].isFull}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            ) : (
+                                <div className='label' >
+                                    <p className=' font-weight-bold'>Is Booked:  {roomData[0].isFull}</p>
+                                </div>
+                            )
 
                         }
 
